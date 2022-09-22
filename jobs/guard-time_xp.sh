f6203a16c9ce884cc46f24b3a41873e4034a998b
Spasky-ecologist <patrice.leveille97@gmail.com>
2022-09-22 12:28:39 -0400

    Changed cpus to 48 and threading to 12 on guard-time_xp R script

--- jobs/guard-time_xp.sh
+++ jobs/guard-time_xp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-monti
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=40
+#SBATCH --cpus-per-task=48
 #SBATCH --mem=32000M
 #SBATCH --time=00-01:30
 #SBATCH --mail-user=leveille.patrice@courrier.uqam.ca
@@ -11,4 +11,4 @@ module load gcc/9.3.0
 module load r-bundle-bioconductor/3.14
 module load r/4.1.2
 #export R_LIBS=~/.local/R/$EBVERSIONR/
-Rscript --no-save --no-restore guard-time_xp.R
+Rscript guard-time_xp.R
