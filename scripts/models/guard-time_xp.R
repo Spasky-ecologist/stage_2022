ad8e6caf7db2b9ba192f1420086f55408c24eac5
Spasky-ecologist <patrice.leveille97@gmail.com>
2022-09-28 16:04:02 -0400

    Changed equal sign for arrows (variable assignation only)

--- scripts/models/guard-time_xp.R
+++ scripts/models/guard-time_xp.R
@@ -21,14 +21,14 @@ library(parallel)
 # Import the data ----------------------------------------------------------
 
 # Folder path Compute Canada
-folder = file.path("/home", "ab991036", "projects", "def-monti", 
+folder <- file.path("/home", "ab991036", "projects", "def-monti", 
                     "ab991036", "stage_2022", "data")
 
 # Import the data
-data = fread(file.path(folder, "02_final-data.csv"),
+data <- fread(file.path(folder, "02_final-data.csv"),
               select = c("match_encode_id", "predator_id", "guard_time_total", "cumul_xp_killer", ))
 
-data = unique(data)
+data <- unique(data)
 
 # ==========================================================================
 # ==========================================================================
@@ -49,7 +49,7 @@ data = unique(data)
 # Standardise the variables (Z-scores) -------------------------------------
 
 #Standardisation function
-standardize = function (x) {(x - mean(x, na.rm = TRUE)) / 
+standardize <- function (x) {(x - mean(x, na.rm = TRUE)) / 
     sd(x, na.rm = TRUE)}
 
 #Use standardisation formula on predator experience and add a new column
@@ -75,7 +75,7 @@ data[, c("Zcumul_xp_killer") :=
 
 # linear model formula -----------------------------------------------------
 
-form_guard = brmsformula(guard_time_total ~ 1 + Zcumul_xp_killer + (1 | predator_id), 
+form_guard <- brmsformula(guard_time_total ~ 1 + Zcumul_xp_killer + (1 | predator_id), 
                          sigma ~ 1 + Zcumul_xp_killer) +
   gaussian()
 
@@ -114,7 +114,7 @@ priors <- c(
 
 
 #Modele complet
-modele_guard_xp = brm(formula = form_guard,
+modele_guard_xp <- brm(formula = form_guard,
                   warmup = 500,
                   iter = 1500,
                   thin = 4,
@@ -139,7 +139,7 @@ saveRDS(modele_guard_xp, file = "guard_time_xp_base_model.rds")
 
 # Capture the session ------------------------------------------------------
 
-session = sessionInfo()
+session <- sessionInfo()
 capture.output(session, file = "session-guard_time-models.txt")
 
 # ==========================================================================
