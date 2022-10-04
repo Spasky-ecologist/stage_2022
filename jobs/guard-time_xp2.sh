efd3dd2479e85c046868a11f7289f4257a62dc9a
Spasky-ecologist <patrice.leveille97@gmail.com>
2022-10-04 16:55:24 -0400

    Added job file for guard-time_xp2 (slope script)

---
+++ jobs/guard-time_xp2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --account=def-monti
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=48
+#SBATCH --mem=32000M
+#SBATCH --time=00-00:15
+#SBATCH --mail-user=leveille.patrice@courrier.uqam.ca
+#SBATCH --mail-type=ALL
+module load StdEnv/2020
+module load gcc/9.3.0
+module load r-bundle-bioconductor/3.14
+module load r/4.1.2
+#export R_LIBS=~/.local/R/$EBVERSIONR/
+Rscript guard-time_xp2.R
