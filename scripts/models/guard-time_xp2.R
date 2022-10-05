95a8fc1f77051eefdd7ced5b949743f4473accd1
Spasky-ecologist <patrice.leveille97@gmail.com>
2022-10-05 14:28:45 -0400

    Changed family to hurdle log_normal

--- scripts/models/guard-time_xp2.R
+++ scripts/models/guard-time_xp2.R
@@ -81,10 +81,10 @@ data[, c("Zcumul_xp_killer") :=
 
 
 #Formula to have the strength of the relation for each player
-form_guard_slope = brmsformula(guard_time_total ~ 1 + Zcumul_xp_killer + (1 | predator_id), 
-                               sigma ~ 1 + Zcumul_xp_killer) + 
-  zero_inflated_poisson()
-    
+form_guard_slope = brmsformula(guard_time_total ~ 1 + Zcumul_xp_killer + (1 + Zcumul_xp_killer | predator_id), 
+                               hu ~ 1 + Zcumul_xp_killer + (1 | predator_id)) +
+  hurdle_lognormal()
+
   
 
 
