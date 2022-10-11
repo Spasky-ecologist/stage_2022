dcb931cc56783b37b62a20164a821ba29874d9ec
Spasky-ecologist <patrice.leveille97@gmail.com>
2022-10-11 17:49:54 -0400

    Added folder for data and added metadata file

--- scripts/model_diagnostics/guard-time_xp_diagnostics.R
+++ scripts/model_diagnostics/guard-time_xp_diagnostics.R
@@ -61,7 +61,7 @@ plot(conditional_effects(model))
 
 
 #La moyenne de l'echantillon (noir) vs les moyennes des sims
-bayesplot_grid(pp_check(model, type = 'stat', stat = mean))
+bayesplot_grid(pp_check(model, type = 'stat', stat = "mean"))
 
 
 #Distribution de notre echantillon vs les sims
