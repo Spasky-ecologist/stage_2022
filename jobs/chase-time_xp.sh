217b5eb9b5ef21216251b7cddc95a420e1af2663
Spasky-ecologist <patrice.leveille97@gmail.com>
2022-11-22 11:27:39 -0500

    Adjusted run time

--- jobs/chase-time_xp.sh
+++ jobs/chase-time_xp.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=48
 #SBATCH --mem=32000M
-#SBATCH --time=00-21:30
+#SBATCH --time=01-05:00
 #SBATCH --mail-user=leveille.patrice@courrier.uqam.ca
 #SBATCH --mail-type=ALL
 module load StdEnv/2020
